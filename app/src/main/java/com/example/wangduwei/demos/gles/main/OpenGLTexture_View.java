2c2b1c94e7a904ea24ccb29b670fef52f1b83e9d
wangduwei <wangdw@flatincbr.com>
2026-01-17 23:27:53 +0800

    add

--- app/src/main/java/com/example/wangduwei/demos/gles/main/OpenGLTexture_View.java
+++ app/src/main/java/com/example/wangduwei/demos/gles/main/OpenGLTexture_View.java
@@ -4,26 +4,26 @@ import android.content.Context;
 import android.opengl.GLSurfaceView;
 import android.util.AttributeSet;
 
-import com.example.lib_gles.render.OpenGLRender;
+import com.example.lib_gles.texture.OpenGLRender7;
 
 /**
- * 这个是空气曲棍球游戏的绘制
+ * 这个是空气曲棍球游戏的绘制-第七章 纹理贴图
  */
-public class OpenGLView extends GLSurfaceView {
-    private GLSurfaceView.Renderer mRender;
+public class OpenGLTexture_View extends GLSurfaceView {
+    private Renderer mRender;
 
-    public OpenGLView(Context context) {
+    public OpenGLTexture_View(Context context) {
         this(context,null);
     }
 
-    public OpenGLView(Context context, AttributeSet attrs) {
+    public OpenGLTexture_View(Context context, AttributeSet attrs) {
         super(context, attrs);
         init(context);
     }
 
     private void init(Context context){
         setEGLContextClientVersion(2);
-        mRender = new OpenGLRender(context);
+        mRender = new OpenGLRender7(context);
         setRenderer(mRender);
     }
 }
